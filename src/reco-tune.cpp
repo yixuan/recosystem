6d701197599f8dfa1a887656028becb5d32c4894
Yixuan Qiu <yixuanq@gmail.com>
2016-06-29 10:33:12 -0400

    model tuning

--- src/reco-tune.cpp
+++ src/reco-tune.cpp
@@ -10,31 +10,25 @@
 #include <Rcpp.h>
 
 #include "mf.h"
+#include "reco-read-data.h"
 
 using namespace mf;
 
 struct TuneOption
 {
-    TuneOption() : param(mf_get_default_param()), nr_folds(5) {}
     mf_parameter param;
-    mf_int nr_folds;
+    mf_int       nr_folds;
+    
+    TuneOption() : param(mf_get_default_param()), nr_folds(5) {}
 };
 
 TuneOption parse_tune_option(SEXP opts_)
 {
     Rcpp::List opts(opts_);
-
     TuneOption option;
 
-    // k, lambda and eta are tuning parameters. They will be set in the main
-    // program. Here we just give some default values.
-
-    // Dimension
-    option.param.k = 10;
-    // Regularization parameter
-    option.param.lambda = 0.1;
-    // Learning rate
-    option.param.eta = 0.1;
+    // k, lambda_* and eta are tuning parameters. They will be set in the main
+    // program.
 
     // Cross validation folds
     option.nr_folds = Rcpp::as<mf_int>(opts["nfold"]);
@@ -52,7 +46,7 @@ TuneOption parse_tune_option(SEXP opts_)
         throw std::invalid_argument("number of threads should be greater than zero");
 
     // Whether to perform NMF or not
-    option.param.do_nmf = Rcpp::as<mf_int>(opts["nmf"]);
+    option.param.do_nmf = Rcpp::as<bool>(opts["nmf"]);
 
     // Verbose or not
     option.param.quiet = !(Rcpp::as<bool>(opts["verbose"]));
@@ -63,89 +57,54 @@ TuneOption parse_tune_option(SEXP opts_)
     return option;
 }
 
-mf_problem read_data(std::string path)
-{
-    mf_problem prob;
-    prob.m = 0;
-    prob.n = 0;
-    prob.nnz = 0;
-    prob.R = nullptr;
 
-    if(path.empty())
-    {
-        return prob;
-    }
 
-    std::ifstream f(path);
-    if(!f.is_open())
-        throw std::runtime_error("cannot open " + path);
-    std::string line;
-    while(std::getline(f, line))
-        prob.nnz++;
-
-    mf_node *R = new mf_node[prob.nnz];
-
-    f.close();
-    f.open(path);
-
-    mf_node N;
-    mf_long idx = 0, lino = 0;
-    for(lino = 0; lino < prob.nnz; lino++)
-    {
-        std::getline(f, line);
-        std::stringstream ss(line);
-
-        ss >> N.u >> N.v >> N.r;
-        if(!ss)
-            continue;
-
-        if(N.u+1 > prob.m)
-            prob.m = N.u+1;
-        if(N.v+1 > prob.n)
-            prob.n = N.v+1;
-        R[idx] = N;
-        idx++;
-    }
-    prob.nnz = idx;
-    prob.R = R;
-
-    return prob;
-}
-
-RcppExport SEXP reco_tune(SEXP train_path_, SEXP opts_tune_, SEXP opts_other_)
+RcppExport SEXP reco_tune(SEXP train_data_, SEXP opts_tune_, SEXP opts_other_)
 {
 BEGIN_RCPP
 
     Rcpp::DataFrame opts_tune(opts_tune_);
-    Rcpp::IntegerVector tune_dim   = opts_tune["dim"];
-    Rcpp::NumericVector tune_cost  = opts_tune["cost"];
-    Rcpp::NumericVector tune_lrate = opts_tune["lrate"];
-    int n = tune_dim.length();
+    Rcpp::IntegerVector tune_dim       = opts_tune["dim"];
+    Rcpp::NumericVector tune_costp_l1  = opts_tune["costp_l1"];
+    Rcpp::NumericVector tune_costp_l2  = opts_tune["costp_l2"];
+    Rcpp::NumericVector tune_costq_l1  = opts_tune["costq_l1"];
+    Rcpp::NumericVector tune_costq_l2  = opts_tune["costq_l2"];
+    Rcpp::NumericVector tune_lrate     = opts_tune["lrate"];
+    mf_long n = tune_dim.length();
     Rcpp::NumericVector rmse(n);
 
     TuneOption option = parse_tune_option(opts_other_);
 
-    std::string train_path = Rcpp::as<std::string>(train_path_);
-    mf_problem tr = read_data(train_path);
+    DataReader* data_reader;
+    // TODO: construct data_reader from train_data
+    
+    mf_problem tr = read_data(data_reader);
 
-    for(int i = 0; i < n; i++)
+    for(mf_long i = 0; i < n; i++)
     {
         if(!option.param.quiet)
         {
-            Rcpp::Rcout << "===== dim = " << tune_dim[i];
-            Rcpp::Rcout << ", cost = " << tune_cost[i];
-            Rcpp::Rcout << ", lrate = " << tune_lrate[i] << " =====" << std::endl;
+            Rcpp::Rcout << "============================"   << std::endl;
+            Rcpp::Rcout << "dim:      " << tune_dim[i]      << std::endl;
+            Rcpp::Rcout << "costp_l1: " << tune_costp_l1[i] << std::endl;
+            Rcpp::Rcout << "costp_l2: " << tune_costp_l2[i] << std::endl;
+            Rcpp::Rcout << "costq_l1: " << tune_costq_l1[i] << std::endl;
+            Rcpp::Rcout << "costq_l2: " << tune_costq_l2[i] << std::endl;
+            Rcpp::Rcout << "lrate:    " << tune_lrate[i]    << std::endl;
         }
         
         // Set value for k, lambda and eta
-        option.param.k      = tune_dim[i];
-        option.param.lambda = tune_cost[i];
-        option.param.eta    = tune_lrate[i];
+        option.param.k         = tune_dim[i];
+        option.param.lambda_p1 = tune_costp_l1[i];
+        option.param.lambda_p2 = tune_costp_l2[i];
+        option.param.lambda_q1 = tune_costq_l1[i];
+        option.param.lambda_q2 = tune_costq_l2[i];
+        option.param.eta       = tune_lrate[i];
 
         rmse[i] = mf_cross_validation(&tr, option.nr_folds, option.param);
         
         if(!option.param.quiet)
-            Rcpp::Rcout << "==============" << std::endl << std::endl;
+            Rcpp::Rcout << "============================" << std::endl << std::endl;
     }
 
     delete[] tr.R;
