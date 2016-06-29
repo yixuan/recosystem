7a423213e5d605db930b201525db7054323e782b
Yixuan Qiu <yixuanq@gmail.com>
2016-06-29 09:52:58 -0400

    model training

--- src/reco-train.cpp
+++ src/reco-train.cpp
@@ -8,115 +8,90 @@
 #include <vector>
 
 #include <Rcpp.h>
-
 #include "mf.h"
+#include "reco-read-data.h"
 
 using namespace mf;
 
-struct TrainOption
+mf_parameter parse_train_option(SEXP opts_)
 {
-    TrainOption() : param(mf_get_default_param()), nr_folds(1), do_cv(false) {}
-    std::string tr_path, va_path, model_path;
-    mf_parameter param;
-    mf_int nr_folds;
-    bool do_cv;
-};
-
-TrainOption parse_train_option(SEXP train_path_,
-                               SEXP model_path_,
-                               SEXP opts_)
-{
-    Rcpp::CharacterVector train_path(train_path_);
-    Rcpp::CharacterVector model_path(model_path_);
     Rcpp::List opts(opts_);
+    mf_parameter param = mf_get_default_param();
 
-    TrainOption option;
-
-    // Regularization parameter
-    option.param.lambda = Rcpp::as<mf_float>(opts["cost"]);
-    if(option.param.lambda < 0)
-        throw std::invalid_argument("regularization parameter should not be smaller than zero");
+    // Regularization parameters
+    param.lambda_p1 = Rcpp::as<mf_float>(opts["costp_l1"]);
+    param.lambda_p2 = Rcpp::as<mf_float>(opts["costp_l2"]);
+    param.lambda_q1 = Rcpp::as<mf_float>(opts["costq_l1"]);
+    param.lambda_q2 = Rcpp::as<mf_float>(opts["costq_l2"]);
+    if(param.lambda_p1 < 0 || param.lambda_p2 < 0 ||
+       param.lambda_q1 < 0 || param.lambda_q2 < 0)
+        throw std::invalid_argument("regularization parameters should not be negative");
 
     // Dimension
-    option.param.k = Rcpp::as<mf_int>(opts["dim"]);
-    if(option.param.k <= 0)
+    param.k = Rcpp::as<mf_int>(opts["dim"]);
+    if(param.k <= 0)
         throw std::invalid_argument("number of factors should be greater than zero");
 
     // Number of iterations
-    option.param.nr_iters = Rcpp::as<mf_int>(opts["niter"]);
-    if(option.param.nr_iters <= 0)
+    param.nr_iters = Rcpp::as<mf_int>(opts["niter"]);
+    if(param.nr_iters <= 0)
         throw std::invalid_argument("number of iterations should be greater than zero");
 
     // Learning rate
-    option.param.eta = Rcpp::as<mf_float>(opts["lrate"]);
-    if(option.param.eta <= 0)
+    param.eta = Rcpp::as<mf_float>(opts["lrate"]);
+    if(param.eta <= 0)
         throw std::invalid_argument("learning rate should be greater than zero");
 
     // Number of threads
-    option.param.nr_threads = Rcpp::as<mf_int>(opts["nthread"]);
-    if(option.param.nr_threads <= 0)
+    param.nr_threads = Rcpp::as<mf_int>(opts["nthread"]);
+    if(param.nr_threads <= 0)
         throw std::invalid_argument("number of threads should be greater than zero");
 
-    // Whether perform NMF or not
-    option.param.do_nmf = Rcpp::as<mf_int>(opts["nmf"]);
+    // Whether to perform NMF or not
+    param.do_nmf = Rcpp::as<bool>(opts["nmf"]);
 
     // Verbose or not
-    option.param.quiet = !(Rcpp::as<bool>(opts["verbose"]));
-
-    // Path of validation set if specified, otherwise an empty string
-    option.va_path = Rcpp::as<std::string>(opts["va_path"]);
-
-    // If validation set is unspecified, use cross validation
-    option.nr_folds = Rcpp::as<mf_int>(opts["nfold"]);
-    if(option.nr_folds > 1)
-        option.do_cv = true;
-
-    // Path to training set
-    option.tr_path = Rcpp::as<std::string>(train_path);
-
-    // Path to model file
-    option.model_path = Rcpp::as<std::string>(model_path);
-
+    param.quiet = !(Rcpp::as<bool>(opts["verbose"]));
+    
     // Whether to copy data matrix or not
-    option.param.copy_data = false;
+    param.copy_data = false;
 
-    return option;
+    return param;
 }
 
-RcppExport SEXP reco_train(SEXP train_path, SEXP model_path, SEXP opts)
+
+
+RcppExport SEXP reco_train(SEXP train_data, SEXP model_path, SEXP opts)
 {
 BEGIN_RCPP
+    
+    DataReader* data_reader;
+    // TODO: construct data_reader from train_data
+    
+    std::string model_file = Rcpp::as<std::string>(model_path);
+    mf_parameter param = parse_train_option(opts);
 
-    TrainOption option = parse_train_option(train_path, model_path, opts);
-
-    mf_problem tr, va;
-    tr = read_problem(option.tr_path);
-    va = read_problem(option.va_path);
-
-    mf_model *model = mf_train_with_validation(&tr, &va, option.param);
-    mf_int status = mf_save_model(model, option.model_path.c_str());
+    mf_problem tr = read_data(data_reader);
+    mf_model* model = mf_train(&tr, param);
+    mf_int status = mf_save_model(model, model_file.c_str());
 
     if(status != 0)
     {
         mf_destroy_model(&model);
-
         delete[] tr.R;
-        delete[] va.R;
 
-        std::string msg = "cannot save model to " + option.model_path;
+        std::string msg = "cannot save model to " + model_file;
         Rcpp::stop(msg.c_str());
     }
 
     Rcpp::List model_param = Rcpp::List::create(
         Rcpp::Named("nuser") = Rcpp::wrap(model->m),
         Rcpp::Named("nitem") = Rcpp::wrap(model->n),
-        Rcpp::Named("nfac") = Rcpp::wrap(model->k)
+        Rcpp::Named("nfac")  = Rcpp::wrap(model->k)
     );
 
     mf_destroy_model(&model);
-
     delete[] tr.R;
-    delete[] va.R;
 
     return model_param;
 
