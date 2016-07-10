207a67b974bd5dbb73961f66d0c351ffd70d0981
Yixuan Qiu <yixuan.qiu@cos.name>
2016-07-10 10:24:26 -0400

    add example for data_memory()

--- R/RecoSys.R
+++ R/RecoSys.R
@@ -234,12 +234,20 @@ RecoSys$methods(
 #'                       \code{TRUE}.}
 #' }
 #' 
-#' @examples train_set = system.file("dat", "smalltrain.txt", package = "recosystem")
+#' @examples ## Training model from a data file
+#' train_set = system.file("dat", "smalltrain.txt", package = "recosystem")
 #' r = Reco()
 #' set.seed(123) # This is a randomized algorithm
-#' r$train(data_file(train_set), opts = list(dim = 20,
-#'                                           costp_l2 = 0.01, costq_l2 = 0.01,
-#'                                           nthread = 2))
+#' r$train(data_file(train_set),
+#'         opts = list(dim = 20, costp_l2 = 0.01, costq_l2 = 0.01, nthread = 1)
+#' )
+#' 
+#' ## Training model from data in memory
+#' train_df = read.table(train_set, sep = " ", header = FALSE)
+#' set.seed(123)
+#' r$train(data_memory(train_df[, 1], train_df[, 2], train_df[, 3]),
+#'         opts = list(dim = 20, costp_l2 = 0.01, costq_l2 = 0.01, nthread = 1)
+#' )
 #' 
 #' @author Yixuan Qiu <\url{http://statr.me}>
 #' @seealso \code{$\link{tune}()}, \code{$\link{output}()}, \code{$\link{predict}()}
